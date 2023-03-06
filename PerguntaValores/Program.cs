559cb7a0ef5d06df1023226143cc42a948a20dc9
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-06 18:05:37 +0000

    Add volume calculation

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = float.Parse(args[0]);
+            double r = float.Parse(args[1]);
+
+            double V = Math.PI * (r * r) * a;
+            Console.WriteLine($"Volume = {V}");
+            
+        }
+    }
+}

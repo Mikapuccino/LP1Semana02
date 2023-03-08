a0f94415a6aa17d2f7945a6f569b4219a724f859
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-08 17:25:27 +0000

    Add comments to PerguntaValores

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,16 +6,22 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {
+            // Asks for a height and adds it to a variable
             Console.WriteLine("Height: ");
             string h = Console.ReadLine();
             double height = double.Parse(h);
 
+            // Asks for a radius and adds it to a variable
             Console.WriteLine("Radius: ");
             string r = Console.ReadLine();
             double radius = double.Parse(r);
 
+            // Calculates volume first and surface area next and
+            // adds them to their respective variables
             double V = Math.PI * Math.Pow(radius, 2) * height;
             double S = (2 * Math.PI) * (radius * (radius + height));
+
+            // Writes the final volume and surface area
             Console.WriteLine($"Volume = {V}");
             Console.WriteLine($"Surface = {S}");
         }

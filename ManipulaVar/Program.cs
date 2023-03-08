5a5ff346cf1193bca8f62b7487fa3ac9ae2ad33e
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-08 17:28:02 +0000

    Add comments to ManipulaVar

--- ManipulaVar/Program.cs
+++ ManipulaVar/Program.cs
@@ -6,10 +6,13 @@ namespace ManipulaVar
     {
         static void Main(string[] args)
         {
+            // Converts first argument into an int and increments it
             int x = int.Parse(args[0]);
             x++;
             Console.WriteLine($"{x}");
 
+            // Converts first argument into an int to become the original
+            // value again and decrements it
             x = int.Parse(args[0]);
             --x;
             Console.WriteLine($"{x}");

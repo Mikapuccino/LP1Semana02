9de88b4b92da4519ed7c066cf57f42021a23b29d
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-08 16:41:40 +0000

    Add auxiliary variables, fix for cycle

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -6,6 +6,17 @@ namespace Triangle
     {
         static void Main(string[] args)
         {
+            // Auxiliary variables
+
+            int size = args[0];
+            char symbol = args[1];
+            
+            // Cycle will repeat until size is bigger than the first argument
+            for (int line = 1; line <= size; line++)
+            {
+                
+            }
+            
             Console.WriteLine("Hello, World!");
         }
     }

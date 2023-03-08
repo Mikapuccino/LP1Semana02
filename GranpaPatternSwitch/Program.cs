21f10764e1b7df59f756c159f77cb9637bb2ad8e
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-08 16:08:20 +0000

    Change sintax of switch case to patterns

--- GranpaPatternSwitch/Program.cs
+++ GranpaPatternSwitch/Program.cs
@@ -6,7 +6,28 @@ namespace GranpaPatternSwitch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Auxiliary variables
+            string question, response;
+
+            // Ask user a question
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Check if it is a known question and determine the
+            // proper answer
+            response = question switch
+            {
+                "How are you?" => "I'm fine, thank you",
+
+                "What's your name?" => "Gran'pa",
+
+                "What's your mission?" => "Destroy mankind!",
+
+                _ => "You got me, I'm not THAT smart!",
+            };
+
+            // Answer properly
+            Console.WriteLine(response);
         }
     }
 }

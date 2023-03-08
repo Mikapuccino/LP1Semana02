1ea0d96ccb4e9034c3ac7a128c8a9d2cc3cbd02d
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-08 17:23:37 +0000

    Fix PerguntaValores

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,11 +6,16 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {
-            double a = float.Parse(args[0]);
-            double r = float.Parse(args[1]);
+            Console.WriteLine("Height: ");
+            string h = Console.ReadLine();
+            double height = double.Parse(h);
 
-            double V = Math.PI * Math.Pow(r, 2) * a;
-            double S = (2 * Math.PI) * (r * (r + a));
+            Console.WriteLine("Radius: ");
+            string r = Console.ReadLine();
+            double radius = double.Parse(r);
+
+            double V = Math.PI * Math.Pow(radius, 2) * height;
+            double S = (2 * Math.PI) * (radius * (radius + height));
             Console.WriteLine($"Volume = {V}");
             Console.WriteLine($"Surface = {S}");
         }

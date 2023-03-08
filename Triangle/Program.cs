cebd422c47a8add62646b3ee57a8d3b14272ec9b
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-08 17:05:47 +0000

    Remove lineSize variable, clean useless comments

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -8,32 +8,16 @@ namespace Triangle
         {
             // Auxiliary variables
 
-            int size = args[0];
-            int lineSize = 1;
-            char symbol = args[1];
+            int size = int.Parse(args[0]);
+            char symbol = char.Parse(args[1]);
             string finalLine = "";
             
             // Cycle will repeat until size is bigger than the first argument
             for (int line = 1; line <= size; line++)
             {
+                // Adds character to end of string and writes the new string
                 finalLine += symbol;
                 Console.WriteLine(finalLine);
-                
-                // Cycle will repeat until the size of the line is bigger
-                // than the number of the line, ex: line 2 has a size of 2
-                //while (lineSize <= line)
-                //{
-                    // Adds the symbol chosen to the end of the string
-                    // a number of times equal to the number of the line
-                //    finalLine += symbol;
-                //    lineSize++;
-                //}
-
-                // Writes the line with correct amount of characters
-                //Console.WriteLine(finalLine);
-
-                // String resets to properly write the next line
-                //finalLine = "";
             }
         }
     }

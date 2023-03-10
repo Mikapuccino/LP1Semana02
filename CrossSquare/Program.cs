444e5e85d78ffd1ffeb1e1bf15fb6b54195d51c6
Nelson <nelsonfcmilheiro@gmail.com>
2023-03-10 13:57:20 +0000

    Add CrossSquare, write first line properly

---
+++ CrossSquare/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CrossSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Auxiliary variables
+
+            int size = int.Parse(args[0]);
+            string finalLine = "";
+            
+            // Cycle will repeat until size is bigger than the first argument
+            for (int line = 1; line <= size; line++)
+            {
+                // Adds character to end of string
+                finalLine += "#";
+            }
+
+            // Writes the first string properly
+            Console.WriteLine(finalLine);
+        }
+    }
+}

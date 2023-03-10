5cfecf7ebafde2b947aa9d65cc3160213e6ebe79
Nelson <nelsonfcmilheiro@gmail.com>
2023-03-10 14:19:10 +0000

    Draw square of specified size

--- CrossSquare/Program.cs
+++ CrossSquare/Program.cs
@@ -20,6 +20,49 @@ namespace CrossSquare
 
             // Writes the first string properly
             Console.WriteLine(finalLine);
+
+            // Reset finalLine to modify later
+            finalLine = "";
+
+            // Cycle will repeat until height is equal to the first argument
+            for (int height = 1; height < size - 1; height++)
+            {
+                // Cycle repeats for each line that needs to be drawn
+                // Chooses the appropriate character to add based on its
+                // position in the line
+                for (int character = 1; character <= size; character++)
+                {
+                    // First character is always a #
+                    if (character == 1)
+                    {
+                        finalLine += "#";
+                    }
+
+                    // Final character is always a #
+                    else if (character == size)
+                    {
+                        finalLine += "#";
+                    }
+
+                    // In between the # there is always empty space
+                    else
+                    {
+                        finalLine += " ";
+                    }
+                }
+
+                Console.WriteLine(finalLine);
+                finalLine = "";
+            }
+
+            for (int line = 1; line <= size; line++)
+            {
+                // Adds character to end of string
+                finalLine += "#";
+            }
+
+            // Writes the final string properly
+            Console.WriteLine(finalLine);
         }
     }
 }
